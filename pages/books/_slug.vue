de4f9c18e732746c1db51c69cc28054c87a8aeeb
Pietro.Caruso <p.caruso86@gmail.com>
2020-10-26 18:50:42 +0100

    component content in books

--- pages/books/_slug.vue
+++ pages/books/_slug.vue
@@ -1,14 +1,19 @@
 <template>
-  <div class="banner" :class="[`banner--${page.category}`]">
-    <div
-      class="container flex items-center justify-around h-full px-4 py-8 mx-auto"
-    >
-      <IconBase :icon-name="page.category" class="h-full" />
-      <div class="text-center">
-        <h1 class="text-5xl" v-text="page.title" />
-        <h1 class="text-3xl" v-text="page.author" />
+  <div>
+    <div class="banner" :class="[`banner--${page.category}`]">
+      <div
+        class="container flex items-center justify-around h-full px-4 py-8 mx-auto"
+      >
+        <IconBase :icon-name="page.category" class="h-full" />
+        <div class="text-center">
+          <h1 class="text-5xl" v-text="page.title" />
+          <h1 class="text-3xl" v-text="page.author" />
+        </div>
       </div>
     </div>
+    <main class="bg-main">
+      <nuxt-content :document="page" class="container px-8" />
+    </main>
   </div>
 </template>
 
@@ -34,5 +39,14 @@ export default {
 }
 .banner--horror {
   @apply bg-gradient-to-r from-horror-dark via-horror-light to-horror-dark;
+}
+</style>
+<style>
+.nuxt-content-editor,
+.nuxt-content-container {
+  @apply bg-transparent;
+}
+.bg-main {
+  background: radial-gradient(circle closest-side, #572361, #17162e);
 }
 </style>
