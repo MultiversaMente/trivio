8fcd675796932eed30ee2d3618fe1288d0b7c484
Pietro.Caruso <p.caruso86@gmail.com>
2020-11-16 13:57:42 +0100

    Site-name

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ export default {
   // },
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'trivio',
+    title: 'Collana Trivio',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
